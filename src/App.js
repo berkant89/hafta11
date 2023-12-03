d22671577c8e0d19ee7b4ee4d166937654d6595b
BERKAN SATIN <berkantsatin@hotmail.com>
2023-12-03 16:34:59 +0300

    mouse ornek state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [items, setItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const addItem = () => setItems(eskiDeger=> [...eskiDeger, `Item ${eskiDeger.length + 1}`]);
+  const updateMousePosition = (e) =>
+    setPosition({ x: e.clientX, y: e.clientY });
 
   return (
-    <div>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-
-      <button onClick={addItem}>Add Item</button>
+    <div style={ {background: "black", color:"white", width: "100%", "height": "100%"} } onMouseMove={updateMousePosition}>
+      <p>Mouse Position: ({position.x}, {position.y})</p>
     </div>
   );
 };
